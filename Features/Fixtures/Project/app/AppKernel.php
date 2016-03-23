98dffea97cfa7d1a2bd573d1142bdfd32de7d2d1
Jakub Zalas <jakub@zalas.pl>
2016-03-23 18:13:50 +0000

    Leverage the micro kernel in tests

--- Features/Fixtures/Project/app/AppKernel.php
+++ Features/Fixtures/Project/app/AppKernel.php
@@ -1,30 +1,27 @@
 <?php
 
-use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Routing\RouteCollectionBuilder;
 
 class AppKernel extends Kernel
 {
+    use MicroKernelTrait;
+
     /**
      * @return array
      */
     public function registerBundles()
     {
-        return array(
+        return [
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Zalas\Bundle\DemoBundle\ZalasDemoBundle()
-        );
-    }
-
-    /**
-     * @return null
-     */
-    public function registerContainerConfiguration(LoaderInterface $loader)
-    {
-        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+            new Zalas\Bundle\DemoBundle\ZalasDemoBundle(),
+        ];
     }
 
     /**
@@ -32,7 +29,7 @@ class AppKernel extends Kernel
      */
     public function getCacheDir()
     {
-        return sys_get_temp_dir().'/ZalasDemoBundle/cache';
+        return sys_get_temp_dir().'/ZalasDemoBundle/cache'.$this->environment;
     }
 
     /**
@@ -41,5 +38,39 @@ class AppKernel extends Kernel
     public function getLogDir()
     {
         return sys_get_temp_dir().'/ZalasDemoBundle/logs';
+    }
+
+    /**
+     * @param RouteCollectionBuilder $routes
+     */
+    protected function configureRoutes(RouteCollectionBuilder $routes)
+    {
+        $routes->import('@ZalasDemoBundle/Controller/', '/', 'annotation');
+    }
+
+    /**
+     * @param ContainerBuilder $c
+     * @param LoaderInterface  $loader
+     */
+    protected function configureContainer(ContainerBuilder $c, LoaderInterface $loader)
+    {
+        $c->loadFromExtension('framework', [
+            'secret' => 'my$ecret',
+            'test' => null,
+            'templating' => ['engines' => ['twig']],
+            'profiler' => [
+                'collect' => false,
+            ],
+        ]);
+        $c->loadFromExtension('monolog', [
+            'handlers' => [
+                'main' => [
+                    'type' => 'stream',
+                    'path' => '%kernel.logs_dir%/%kernel.environment%.log',
+                    'level' => 'debug',
+                ],
+            ],
+        ]);
+        $c->loadFromExtension('twig', ['debug' => '%kernel.debug%', 'strict_variables' => '%kernel.debug%']);
     }
 }
